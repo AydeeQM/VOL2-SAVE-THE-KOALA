6f62b09685255a2a9afe38814f6687ca7238e9f8
aydee <aydee.qm@gmail.com>
2017-09-01 15:29:08 -0500

    koala segunda version con efectos

---
+++ js/js.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.myCarousel').carousel({
+        interval: 3000
+    });
+});
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
+
+
+
